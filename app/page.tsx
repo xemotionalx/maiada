eae8230acb8b8470b6261da406a70e72a5fcba23
MaiAda <maiada.carpano@croissant.com>
2025-08-16 11:05:34 -0400

    ascii speech bubble

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AsciiSpeechBubble from "@/components/speech-bubble";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        hello world!
+        <AsciiSpeechBubble />
         <Image
           className={styles.unicorn}
           src="/unicorn_baby.gif"
