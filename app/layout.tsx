6444a34f70f9ee0950fed7c8ec335d73b5a4a15d
MaiAda <maiada.carpano@croissant.com>
2026-06-27 14:41:49 -0400

    fix layout issues on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "maiada",
   description: "hi and welcome to my little corner of the internet",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
